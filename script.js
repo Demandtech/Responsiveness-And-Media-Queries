113fafa6fb45b7a942684a92e9de14c857e0fcb7
Demandtech <demandwork247@gmail.com>
2022-06-20 11:41:57 +0100

    Javascript validation and error image added

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ let form = document.getElementById('form');
 
 form.addEventListener('submit', function(e){
    e.preventDefault();
-   let firstNameError = document.getElementById("fname-error");
    let firstName = document.getElementById("first-name");
-   let lastNameError = document.getElementById("lname-error");
+   let firstNameError = document.getElementById("fname-error");
+   let firstNameErrorImage = document.getElementById("fname-error-img");
+
    let lastName = document.getElementById("last-name");
+   let lastNameError = document.getElementById("lname-error");
+   let lastNameErrorImage = document.getElementById("lname-error-img");
+   
    let email = document.getElementById("email");
    let emailError = document.getElementById("email-error");
+   let emailErrorImage = document.getElementById("email-error-img");
+
    let password = document.getElementById("password");
    let passwordError = document.getElementById("password-error");
+   
 
    if(firstName.value == ""){
      firstName.style.borderColor = "hsl(0, 100%, 74%)";
